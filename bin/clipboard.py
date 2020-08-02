f6aa2c134ce56f3d3b73e2dfdf17e5bee5026da3
Bright@E49 <brt2@qq.com>
2020-08-02 20:51:31 +0800

    bugfix: 修改一个变量名str_pip的拼写错误，导致图像粘贴失败

--- bin/clipboard.py
+++ bin/clipboard.py
@@ -25,6 +25,19 @@ def grabclip_img(cb):
 
 grabclipboard_byQt = grabclip_img
 
+def main_byQt(path_save):
+    img = grabclip_img(cb)
+    if img:
+        save_clipboard_image(path_save, img)
+        return "img"
+
+    text = grabclip_text(cb)
+    # 验证是否为图像链接
+    if text.startswith("http"):  # ?? 这里验证并不完整...
+        return text
+
+    return "err"
+
 
 if __name__ == "__main__":
     import sys
@@ -33,34 +46,12 @@ if __name__ == "__main__":
     cb = QGuiApplication.clipboard()
     # cb.dataChanged.connect()
 
-    # buff = ''
-    # while True:
-    #     x = sys.stdin.read(1)
-    #     if not x:
-    #         break
-    #     buff += x
-    #     if buff[-1] == "\n":
-    #         path_save = buff[:-1]
-    #         buff = ''
-    #         # print(path_save, end="")
-
     for path_save in sys.stdin:
-        # sys.stderr.write(path_save)
-        # if path_save == "/home/brt/quit.jpg\n":
-        #     break
-        str_pip = "err"
-        img = grabclipboard_byQt(cb)
-        if img:
-            save_clipboard_image(path_save.strip(), img)
-            str_pipe = "img"
-        else:
-            text = grabclip_text(cb)
-            # 验证是否为图像链接
-            if text.startswith("http"):  # ?? 这里验证并不完整...
-                str_pipe = text
+        # if path_save == "/home/brt/quit.jpg\n": pass
+        str_pipe = main_byQt(path_save.strip())
 
-        # sys.stdout.write(str_pipe + "\n")  # 必须添加换行符
-        print(str_pipe)
+        # print(str_pipe)
+        sys.stdout.write(str_pipe + "\n")  # 必须添加换行符
         sys.stdout.flush()  # 及时清空缓存
 
     # app.exec()
