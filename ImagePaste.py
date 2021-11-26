291ac5c0e3eb769cfedae853ec261d4e430c0425
Bright@huawei <brt2@qq.com>
2021-11-26 17:21:02 +0800

    bugfix: insert position error

--- ImagePaste.py
+++ ImagePaste.py
@@ -149,7 +149,7 @@ class ImagePasteCommand(ImageCmdInterface, sublime_plugin.TextCommand):
                     tmp_label = md5obj.hexdigest()
 
                     self.view.insert(edit, pos.begin(), "![][%s]" % tmp_label)
-                    self.view.insert(edit, pos.end(), "\n\n[%s]:data:image/png;base64,%s" % (tmp_label, img_base64.decode()))
+                    self.view.insert(edit, self.view.size(), "\n\n[%s]:data:image/png;base64,%s" % (tmp_label, img_str.decode()))
             else:
                 self.view.insert(edit, pos.begin(), "%s" % img_str)
             # only the first cursor add the path
